fb3c6cb796f3a87110d86bf0f4c92a3261fdbd95
AYUSH KUMAR SINGH <ayushsinghzzzzz38@gmail.com>
2025-10-08 12:41:09 +0530

    Update server.js for file payload

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json()); // ← Ensure this is present to parse JSON bodies
 
 app.use(helmet());
